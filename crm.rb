9b76aa75fa7a70f552ee964878291fc7fc768489
Rolande Saliba <rolande.saliba@gmail.com>
2015-01-27 15:55:13 -0500

    added the form and stuff :)

--- crm.rb
+++ crm.rb
@@ -8,10 +8,18 @@ get '/' do
 end
 
 get '/contacts' do
+	@contacts = []
+	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+	@contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+	@contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
+
 	erb :contacts
 end
 
 get '/contacts/new' do
 	erb :new
 end
+
+# post ''
 
